cd9cbd65b6644778a59a14f20121e4b31f8fbe2c
unknown <anshu2lynx@gmail.com>
2024-04-15 11:21:18 +0530

    first commit

---
+++ day4/script.js
@@ -0,0 +1,116 @@
+console.log("Hello, world!");
+//object type
+
+ let object ={
+Name:"john",
+Age:21,
+number:123456789
+};
+console.log(object);
+
+
+let age= 25;
+if(age>10){
+    console.log("you can vote")
+}
+else if(age>18){
+
+}
+else{
+    console.log("you can't vote")
+
+}
+let person1 ={
+    Name: "gaurav",
+    age:23,
+}
+console.log(object);
+
+let i=0;
+const ar =[1,2,3,4,5,6,7];
+while(i<ar.lenth){
+    i=i+1;
+    console.log(ar[i]);
+    i=i+1;
+}
+ for(let i =0;i<10;++i)
+{
+console.log(i);
+}
+
+
+
+let array = [1, 2, 3, 4];
+for (let i = 0; i < array.length; ++i) {
+    console.log(array[i]);
+}
+
+
+
+  const list =[ 'a','b','c','d','e','f'];
+for(const value of list){
+    console.log(value);
+}
+   function add (a,b)
+{
+    let sum =a+b;
+
+}
+add(10,12);
+add(12,123);
+add(234,131);
+add(2904,4245);
+add(2343,3);
+
+
+
+function sub(a,b)
+
+{
+    let diff =a-b;
+    return diff
+}
+let diff =sub(120,10);
+console.log (diff);
+110  
+
+
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log("The sum of", a, "and", b, "is:", sum);
+
+
+let a = 10;
+let b = 5;
+let diff = a - b;
+console.log("The difference between", a, "and", b, "is:", diff);
+
+
+
+
+
+let a = 5;
+let b = 10;
+let product = a * b;
+console.log("The product of", a, "and", b, "is:", product);
+
+
+let a = 10;
+let b = 5;
+let quo = a / b;
+console.log("The quotient of", a, "and", b, "is:", quo);
+
+
+
+let a = 10;
+let b = 3;
+let re = a % b;
+console.log("The remainder of", a, "divided by", b, "is:", re);
+
+
+
+
+
+
